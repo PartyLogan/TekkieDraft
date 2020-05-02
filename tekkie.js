707f621ddcaef9c881fd689a0e50fed9b83b8420
PartyLogan <l.nicol.dickson@gmail.com>
2020-05-02 15:24:42 +1000

    Update tekkie.js
    Remove window.location="indexOrig.html from pass1 check before stream

--- tekkie.js
+++ tekkie.js
@@ -457,6 +457,8 @@ function playmus() {
       password = prompt('Enter password to view page: ',' ');
       if(password === pass1){
           alert('Correct password, click ok to enter');
+          window.location="indexOrig.html
+          
           return true;
       }else if(password === pass2){
         alert('Correct password, click ok to enter');
